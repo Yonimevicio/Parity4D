ebb39f0421c83288d99f2fe683c5f00a8b181445
Cristian <chuloesmyperro@gmail.com>
2020-11-02 18:33:11 +0100

    axis

--- ModuleRender.cpp
+++ ModuleRender.cpp
@@ -4,7 +4,7 @@
 #include "ModuleWindow.h"
 #include "SDL.h"
 #include "GL/glew.h"
-
+#include "MathGeoLib/Geometry/Frustum.h"
 ModuleRender::ModuleRender()
 {
 }
@@ -49,7 +49,7 @@ update_status ModuleRender::PreUpdate()
 	int screen_width, screen_height;
 	SDL_GetWindowSize(App->window->window, &screen_width, &screen_height);
 
-	glClearColor(0.1f, 0.5f, 0.1f, 1.0f);
+	glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	return UPDATE_CONTINUE;
@@ -58,6 +58,65 @@ update_status ModuleRender::PreUpdate()
 // Called every draw update
 update_status ModuleRender::Update()
 {
+	glLineWidth(1.0f); 
+	float d = 200.0f;
+	glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
+	glBegin(GL_LINES); 
+	for (float i = -d; i <= d; i += 1.0f) { 
+		glVertex3f(i, 0.0f, -d); 
+		glVertex3f(i, 0.0f, d); 
+		glVertex3f(-d, 0.0f, i); 
+		glVertex3f(d, 0.0f, i); }
+	glEnd();
+	glLineWidth(2.0f); 
+	glBegin(GL_LINES);
+	// red X
+	glColor4f(1.0f, 0.0f, 0.0f, 1.0f);
+	glVertex3f(0.0f, 0.0f, 0.0f); 
+	glVertex3f(1.0f, 0.0f, 0.0f);
+	glVertex3f(1.0f, 0.1f, 0.0f); 
+	glVertex3f(1.1f, -0.1f, 0.0f);
+	glVertex3f(1.1f, 0.1f, 0.0f); 
+	glVertex3f(1.0f, -0.1f, 0.0f);
+	// green Y
+	glColor4f(0.0f, 1.0f, 0.0f, 1.0f);
+	glVertex3f(0.0f, 0.0f, 0.0f); 
+	glVertex3f(0.0f, 1.0f, 0.0f);
+	glVertex3f(-0.05f, 1.25f, 0.0f); 
+	glVertex3f(0.0f, 1.15f, 0.0f);
+	glVertex3f(0.05f, 1.25f, 0.0f); 
+	glVertex3f(0.0f, 1.15f, 0.0f);
+	glVertex3f(0.0f, 1.15f, 0.0f); 
+	glVertex3f(0.0f, 1.05f, 0.0f);
+	// blue Z
+	glColor4f(0.0f, 0.0f, 1.0f, 1.0f);
+	glVertex3f(0.0f, 0.0f, 0.0f); 
+	glVertex3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(-0.05f, 0.1f, 1.05f); 
+	glVertex3f(0.05f, 0.1f, 1.05f);
+	glVertex3f(0.05f, 0.1f, 1.05f); 
+	glVertex3f(-0.05f, -0.1f, 1.05f);
+	glVertex3f(-0.05f, -0.1f, 1.05f); 
+	glVertex3f(0.05f, -0.1f, 1.05f);
+	glEnd();
+	glLineWidth(1.0f);
+
+	Frustum frustum; 
+	frustum.SetKind(FrustumSpaceGL, FrustumRightHanded); 
+	frustum.SetViewPlaneDistances(0.1f, 200.0f); 
+	frustum.SetHorizontalFovAndAspectRatio(0.01745 * 90.0f, 1.3f);
+	frustum.SetPos(float3(0, 1, -2)); 
+	frustum.SetFront(float3::unitZ); 
+	frustum.SetUp(float3::unitY); 
+	float4x4 projectionGL = frustum.ProjectionMatrix().Transposed();
+	float4x4 view = frustum.ViewMatrix();
+	view.Transpose();
+	
+	glMatrixMode(GL_PROJECTION);
+	glLoadMatrixf(*projectionGL.v);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadMatrixf(*view.v);
 
 	return UPDATE_CONTINUE;
 }
